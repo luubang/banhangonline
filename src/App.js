6e1f51d3fa39e4344086d0940754c5434f38ab07
unknown <hedgehogku@gmail.com>
2019-09-09 21:35:39 +0700

    update app with shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import HomePage from "./pages/homepage/homepage.component";
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,7 +6,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
     <div className={`${size} menu-item`}>
 
         <div className="background-image"
-            style={{ backgroundImage: `url(${imageUrl})` }}>
+            style={ {backgroundImage: `url(${imageUrl})` }}>
         </div>
 
         <div className="content">
