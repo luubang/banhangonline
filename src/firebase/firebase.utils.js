498b1240ed2100bc1c12566f49be7c4b4ce18bcf
unknown <hedgehogku@gmail.com>
2019-10-03 17:26:15 +0700

    updated

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyDQBr1i4PmGTdaASEEp5zPjH4Of3a-3aTA",
+  authDomain: "banhangonline-db.firebaseapp.com",
+  databaseURL: "https://banhangonline-db.firebaseio.com",
+  projectId: "banhangonline-db",
+  storageBucket: "banhangonline-db.appspot.com",
+  messagingSenderId: "726271593547",
+  appId: "1:726271593547:web:9c2eb2ff8861984bf091ce"
 };
 
 firebase.initializeApp(config);
@@ -22,7 +22,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const snapShot = await userRef.get();
 
   if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
+    const {
+      displayName,
+      email
+    } = userAuth;
     const createdAt = new Date();
     try {
       await userRef.set({
@@ -43,7 +46,9 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
